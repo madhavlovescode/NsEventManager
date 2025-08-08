7d64bd1a1b6809b0924e70a663f0e3d415cc0b56
Madhav Vyas <vmredmi45@gmail.com>
2025-08-08 12:19:25 +0530

    Add files via upload

---
+++ ns_event_manager/Classes/Domain/Validator/TitleValidator.php
@@ -0,0 +1,23 @@
+<?php
+    
+    namespace NITSAN\NsEventManager\Domain\Validator ;
+    use TYPO3\CMS\Core\Resource\FileReference;
+    use TYPO3\CMS\Extbase\Validation\Validator\AbstractValidator;
+    use TYPO3\CMS\Core\Resource\FileReference as CoreFileReference;
+
+    final class TitleValidator extends AbstractValidator
+    {
+        protected function isValid($qq): void
+        {
+ 
+            if (str_starts_with( $qq,'_')) {
+
+                $errorString = 'The title may not start with an underscore. ';
+                $this->addError($errorString, 1297418976);
+              
+            }
+        
+        }
+    }
+
+?>
